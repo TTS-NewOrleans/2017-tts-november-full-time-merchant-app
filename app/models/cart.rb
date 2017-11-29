0ef891fddd2b811d4733acd724778d46a19de24b
Ryan Hageman <improv42@gmail.com>
2017-11-29 10:13:20 -0600

    Add Order Model and setup relationships. Change cart to update quantity instead of making new line items.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,12 +1,26 @@
+# Cart Model
 class Cart < ApplicationRecord
   has_many :line_items, dependent: :destroy
+
+  def add_product(product_id)
+    product = Product.find(product_id)
+    current_item = line_items.find_by(product_id: product_id)
+
+    if current_item
+      current_item.quantity += 1
+    else
+      current_item = line_items.build(product: product, price: product.price)
+    end
+
+    current_item
+  end
 
   def subtotal
     # The Ruby Way
     # line_items.to_a.sum { |item| item.total }
 
     # The SQL way (much faster!)
-    line_items.select("SUM(quantity * price) AS sum")[0].sum
+    line_items.select('SUM(quantity * price) AS sum')[0].sum
   end
 end
 
